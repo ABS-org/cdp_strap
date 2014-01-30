e0c4969f5617e4403176b8a7e93492ae4c6907ce
Feel Fine <danielfeelfine@gmail.com>
2014-01-30 20:34:31 -0300

    Bump html5shiv

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -16,10 +16,6 @@
   <!-- Dynamic Styles -->
   <?php print $styles; ?>
 
-  <!-- HTML5 element support for IE6-8 -->
-  <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
