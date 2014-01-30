8175c9252e32f91eac824dbb51a36fcebe562f1a
Feel Fine <danielfeelfine@gmail.com>
2014-01-30 20:39:27 -0300

    Fix initial indent

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -4,53 +4,53 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
-<head profile="<?php print $grddl_profile; ?>">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head profile="<?php print $grddl_profile; ?>">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Dynamic HEAD values -->
-  <?php print $head; ?>
+    <!-- Dynamic HEAD values -->
+    <?php print $head; ?>
 
-  <title><?php print $head_title; ?></title>
+    <title><?php print $head_title; ?></title>
 
-  <!-- Dynamic Styles -->
-  <?php print $styles; ?>
+    <!-- Dynamic Styles -->
+    <?php print $styles; ?>
 
-</head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <!--[if lt IE 7]>
-    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-  <![endif]-->
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
-  <div id="barra-brasil">
-      <div class="barra">
-          <ul>
-              <li><a target="_blank" title="Acesso à informação" class="ai" href="http://www.acessoainformacao.gov.br">www.sic.gov.br</a></li>
-              <li><a target="_blank" title="Portal de Estado do Brasil" class="brasilgov" href="http://www.brasil.gov.br">www.brasil.gov.br</a></li>
-          </ul>
-      </div>
-  </div>
+  </head>
+  <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+    <!--[if lt IE 7]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <div id="skip-link">
+      <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    </div>
+    <div id="barra-brasil">
+        <div class="barra">
+            <ul>
+                <li><a target="_blank" title="Acesso à informação" class="ai" href="http://www.acessoainformacao.gov.br">www.sic.gov.br</a></li>
+                <li><a target="_blank" title="Portal de Estado do Brasil" class="brasilgov" href="http://www.brasil.gov.br">www.brasil.gov.br</a></li>
+            </ul>
+        </div>
+    </div>
 
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
 
-  <div id="help-modal-info" class="modal" tabindex="-1" role="dialog" aria-labelledby="help-modal-info" aria-hidden="true"></div><!-- /.modal -->
+    <div id="help-modal-info" class="modal" tabindex="-1" role="dialog" aria-labelledby="help-modal-info" aria-hidden="true"></div><!-- /.modal -->
 
-  <!-- Scripts at bottom of page for fast loading -->
-  <?php print $scripts; ?>
+    <!-- Scripts at bottom of page for fast loading -->
+    <?php print $scripts; ?>
 
-  <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-  <script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='//www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X');ga('send','pageview');
-  </script>
-  
-</body>
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='//www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+    
+  </body>
 </html>
