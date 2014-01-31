41c9e910818b1bddd8914f5e33bf7be862e6e025
Feel Fine <danielfeelfine@gmail.com>
2014-01-31 07:50:12 -0300

    Init includes path
    with bar-top-brasilgov

---
+++ templates/includes/bar-top-brasilgov.php
@@ -0,0 +1,17 @@
+<div id="bar-top-brasilgov">
+  <nav class="bar-top-brasilgov">
+    <ul>
+      <li>
+      	<a target="_blank" title="Acesso à informação" class="ai" href="http://www.acessoainformacao.gov.br">
+      		www.sic.gov.br
+	      </a>
+			</li>
+
+      <li>
+      	<a target="_blank" title="Portal de Estado do Brasil" class="brasilgov" href="http://www.brasil.gov.br">
+      		www.brasil.gov.br
+      	</a>
+      </li>
+    </ul>
+  </nav>
+</div>
