f5d698373349e68100a0c64640561b64f447908d
Feel Fine <danielfeelfine@gmail.com>
2014-02-02 18:42:01 -0300

    [gh-pages] Init dist main.js
    with classie.js and function init of nav-horizontal menu

---
+++ cp-strap/dist/website/js/main.js
@@ -0,0 +1,137 @@
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+( function( window ) {
+
+'use strict';
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
+
+/**
+ * cbpHorizontalMenu.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpHorizontalMenu = (function() {
+
+	var $listItems = $( '#nav-horizontal > ul > li' ),
+		$menuItems = $listItems.children( 'a' ),
+		$body = $( 'body' ),
+		current = -1;
+
+	function init() {
+		$menuItems.on( 'click', open );
+		$listItems.on( 'click', function( event ) { event.stopPropagation(); } );
+	}
+
+	function open( event ) {
+
+		if( current !== -1 ) {
+			$listItems.eq( current ).removeClass( 'nav-horizontal-submenu-open' );
+		}
+
+		var $item = $( event.currentTarget ).parent( 'li' ),
+			idx = $item.index();
+
+		if( current === idx ) {
+			$item.removeClass( 'nav-horizontal-submenu-open' );
+			current = -1;
+		}
+		else {
+			$item.addClass( 'nav-horizontal-submenu-open' );
+			current = idx;
+			$body.off( 'click' ).on( 'click', close );
+		}
+
+		return false;
+
+	}
+
+	function close( event ) {
+		$listItems.eq( current ).removeClass( 'nav-horizontal-submenu-open' );
+		current = -1;
+	}
+
+	return { init : init };
+
+})();
+$(function() {
+  cbpHorizontalMenu.init();
+});
